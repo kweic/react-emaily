88376edd653014c30ba04cd3b65697d940f6b48c
kweichel <kevinjweichel@gmail.com>
2019-01-02 10:27:31 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+//run with 'node index.js'
+//ctrl + c to stop node
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+//get get info
+//post send info
+//put update all the properties of something
+//delete delete something
+//patch update one or two properties of something
+
+const PORT = process.env.PORT || 5000; //port declared by heroku
+app.listen(PORT);
+//instructs express to tell node.js (the runtime) to listen to this port
