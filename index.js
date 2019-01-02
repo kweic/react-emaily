adc2357bbe456837341831261bdd4eee46ed7470
kweichel <kevinjweichel@gmail.com>
2019-01-02 15:06:32 -0500

    add google oauth

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
+
+//google strategy has internal identifier of 'google'
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token: '+ accessToken);
+        console.log('refresh token: '+refreshToken);
+        console.log(profile);
+    })
+);
+
+app.get(
+    '/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 //run with 'node index.js'
 //ctrl + c to stop node
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+// app.get('/', (req, res) => {
+//     res.send({hi: 'there'});
+// });
+
+
 
 //get get info
 //post send info
@@ -22,3 +50,4 @@ app.listen(PORT);
 // 'heroku create'
 // outputs the url & the git link
 // git push heroku master
+// heroku open to open the browser from command line to this location
