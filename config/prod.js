6bcb3ff6452b4fb7805902c42eea020aea5e51aa
kweichel <kevinjweichel@gmail.com>
2019-01-17 12:29:07 -0500

    add payments option using Stripe

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,8 @@ module.exports = {
     cookieKey: process.env.COOKIE_KEY,
 
     facebookClientID: process.env.FACEBOOK_CLIENT_ID,
-    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
